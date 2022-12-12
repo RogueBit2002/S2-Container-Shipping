3195248e1e139ef765648ad8a9557130af91572e
RogueBit2002 <laurens@kruis.name>
2022-12-12 08:46:27 +0100

    Started working on the algorithm

--- Container Shipping/src/Port.cs
+++ Container Shipping/src/Port.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,22 +9,49 @@ namespace LaurensKruis.ContainerShipping.src
 {
     public static class Port
     {
+        private enum Side : int
+        {
+            Left = -1,
+            Right = 1
+        }
+
+
+
         public static void LoadShip(Ship ship, IEnumerable<Container> containers)
         {
 
+            
+            containers = containers.OrderByDescending(containers => containers.Weight);
+
+            
+            
         }
 
 
-
-        private static void LoadShip_Even()
+        private static (IEnumerable<Container> left, IEnumerable<Container> right) Split(IEnumerable<Container> containers)
         {
+            List<Container> containersLeft = new List<Container>();
+            List<Container> containersRight = new List<Container>();
 
+            foreach (var container in containers)
+            {
+                if (containersLeft.GetWeight() > containersRight.GetWeight())
+                    containersRight.Add(container);
+                else
+                    containersLeft.Add(container);
+            }
+
+            return (containersLeft.AsReadOnly(), containersRight.AsReadOnly());
         }
 
-        private static void LoadShip_Uneven(Ship ship, IEnumerable<Container> containers)
+        private static void FillSide(Ship ship, IEnumerable<Container> containers, Side side)
         {
+            int direction = (int) side;
 
+            
         }
+
+        public static float GetWeight(this IEnumerable<Container> list) => list.Sum(container => container.Weight);
         
     }
 }
--- Container Shipping/src/Ship.cs
+++ Container Shipping/src/Ship.cs
@@ -15,6 +15,8 @@ namespace LaurensKruis.ContainerShipping
 
         public Ship(int width, int height)
         {
+            if (width <= 0 || height <= 0)
+                throw new ArgumentException("Ship capacity should be atleast 1");
             this.width = width;
             this.height = height;
 
@@ -27,6 +29,7 @@ namespace LaurensKruis.ContainerShipping
         public Container this[int x, int y]
         {
             get => grid[x][y];
+            internal set => grid[x][y] = value;
         }
     }
 }
